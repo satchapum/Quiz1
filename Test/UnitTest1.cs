159bf3b0d0eb209b2d2b42408cda3139e66eb5ae
Brazil <satchapum.mail@mail.kmutt.ac.th>
2022-09-16 12:24:37 +0700

    v2

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -3,8 +3,29 @@ namespace Test;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void EmployeeInfoTest()
     {
-
+        int person = 1;
+        string type = EmployeeInfo.type(person);
+        string name = EmployeeInfo.name(person);
+        string surname = EmployeeInfo.surname(person);
+        double age = EmployeeInfo.age(person);
+        double money = EmployeeInfo.money(person);
+        EmployeeInfo Info = new EmployeeInfo(type,name,surname,age,money);
+        
+        Assert.Equal(EmployeeInfo.type(person), "พนักงานประจำร้าน");
+    }
+    [Fact]
+    public void EmployeeInfoTest()
+    {
+        int person = 1;
+        string type = EmployeeInfo.type(person);
+        string name = EmployeeInfo.name(person);
+        string surname = EmployeeInfo.surname(person);
+        double age = EmployeeInfo.age(person);
+        double money = EmployeeInfo.money(person);
+        EmployeeInfo Info = new EmployeeInfo(type,name,surname,age,money);
+        
+        Assert.Equal(EmployeeInfo.type(person), "พนักงานประจำร้าน");
     }
 }
